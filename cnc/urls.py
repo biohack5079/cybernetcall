f0c6364debfe0b5d6cbe5f6010205634fc1da89d
biohack5079 <biohack5079@gmail.com>
2025-04-27 06:31:41 +0900

    change

--- cnc/urls.py
+++ cnc/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from cnc import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.IndexView.as_view(), name="cnc/index.html"),
 ]
 
