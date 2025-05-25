272f18abd837758bc773f645c0534323cc2cdeef
biohack5079 <biohack5079@gmail.com>
2025-05-25 13:45:56 +0900

    changed

--- signaling/consumers.py
+++ signaling/consumers.py
@@ -15,7 +15,8 @@ class SignalingConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         logger.info(f"WebSocket connection closed for {self.channel_name} (UUID: {self.user_uuid}), code: {close_code}")
         if self.user_uuid and self.user_uuid in user_uuid_to_channel:
-            del user_uuid_to_channel[self.user_uuid]
+            if self.user_uuid in user_uuid_to_channel:
+                del user_uuid_to_channel[self.user_uuid]
             logger.info(f"Removed user {self.user_uuid} from tracking.")
 
             await self.broadcast({
