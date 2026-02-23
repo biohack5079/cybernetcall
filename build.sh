e6ac3c7d3f2d57909c02ea897751456110ca5155
biohack5079 <biohack5079@gmail.com>
2026-02-24 00:26:03 +0900

    change

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-# Render Redis (rediss://) へのSSL接続に必要な依存関係をインストールします
-pip install "redis[ssl]"
-
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
